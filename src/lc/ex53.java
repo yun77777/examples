677be01e98c16f2f4f7d352af3ce33af804c7bf8
yun77777 <victoriousyk@naver.com>
2021-06-30 22:07:58 +0900

    “m”

--- src/lc/ex53.java
+++ src/lc/ex53.java
@@ -1,29 +1,24 @@
 package lc;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ex53 {
-	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		String s=sc.nextLine();
-//		String ss=sc.nextLine();
-		int[] a= {-2,1,-3,4,-1,2,1,-5,4};
-		System.out.println(maxSubArray(a));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(lengthOfLastWord(br.readLine()));
+		br.close();
 	}
 
-    public static int maxSubArray(int[] nums) {
-    	if(nums.length==1) return nums[0];
-    	int max=nums[0],sum=0;
-    	
-    	for (int i = 0; i < nums.length; i++) {
-    		sum=0;
-    		for (int j = i; j < nums.length; j++) {
-				sum+=nums[j];
-				if(sum>max)
-					max=sum;
-			}
-		}
-    	
-    	return max;
+    public static int lengthOfLastWord(String s) {
+    	if(s.length()==0||s.equals(" ")||s==null||s.trim().length()==0) return 0;
+    	String[] arr=s.split(" ");
+//    	for (int i = 0; i < arr.length; i++) {
+//			System.err.println(arr[i]);
+//		}
+//    	System.err.println("last:"+arr[arr.length-1]);
+    	int result=arr[arr.length-1].length();
+    	return result;
     }
 }
---
+++ src/lc/ex58.java
@@ -0,0 +1,29 @@
+package lc;
+
+import java.util.Scanner;
+
+public class ex58 {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		String s=sc.nextLine();
+//		String ss=sc.nextLine();
+		int[] a= {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(maxSubArray(a));
+	}
+
+    public static int maxSubArray(int[] nums) {
+    	if(nums.length==1) return nums[0];
+    	int max=nums[0],sum=0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		sum=0;
+    		for (int j = i; j < nums.length; j++) {
+				sum+=nums[j];
+				if(sum>max)
+					max=sum;
+			}
+		}
+    	
+    	return max;
+    }
+}
