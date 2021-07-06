9be2293701cd00a8702c77c366a0ca8497f722ed
yun77777 <victoriousyk@naver.com>
2021-07-06 21:59:53 +0900

    “m”

---
+++ src/lc/ex111.java
@@ -0,0 +1,46 @@
+package lc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class ex111 {
+	public static void main(String[] args) {
+//		Scanner sc=new Scanner(System.in);
+//		int T=sc.nextInt();
+//		int[] nums=new int[T];
+//		int[] result=new int[2];
+//		int target=0;
+//		for(int i=0;i<T;i++) {
+//			nums[i]=sc.nextInt();
+//		}
+//		target=sc.nextInt();
+//		result=twoSum(nums,target);
+//		for(int i=0;i<T;i++) {
+//			System.out.println(result[i]);
+//		}
+			
+	}
+	/**
+	 * Definition for a binary tree node.
+	 */
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left,right) + 1;
+       
+    }
+}
