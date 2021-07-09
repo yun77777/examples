ee3f68b8dc931546f86e13165e6a6ef2fdae5782
yun77777 <victoriousyk@naver.com>
2021-07-09 23:37:46 +0900

    “m”

--- src/lc/ex118.java
+++ src/lc/ex118.java
@@ -21,26 +21,18 @@ public class ex118 {
 //		}
 			
 	}
-	/**
-	 * Definition for a binary tree node.
-	 */
-	public class TreeNode {
-	      int val;
-	      TreeNode left;
-	      TreeNode right;
-	      TreeNode() {}
-	      TreeNode(int val) { this.val = val; }
-	      TreeNode(int val, TreeNode left, TreeNode right) {
-	          this.val = val;
-	          this.left = left;
-	          this.right = right;
-	      }
-	  }
-	public boolean hasPathSum(TreeNode root, int sum) {
-        if(root == null) return false;
-    
-        if(root.left == null && root.right == null && sum - root.val == 0) return true;
-    
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
-    }
+	public List<List<Integer>> generate(int numRows)
+	{
+		List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+		ArrayList<Integer> row = new ArrayList<Integer>();
+		for(int i=0;i<numRows;i++)
+		{
+			row.add(0, 1);
+			for(int j=1;j<row.size()-1;j++)
+				row.set(j, row.get(j)+row.get(j+1));
+			allrows.add(new ArrayList<Integer>(row));
+		}
+		return allrows;
+		
+	}
 }
