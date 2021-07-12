9d2c81c273ace4007dad1f6f2a80521386465b46
yun77777 <victoriousyk@naver.com>
2021-07-12 19:41:05 +0900

    “m”

--- src/lc/ex119.java
+++ src/lc/ex119.java
@@ -2,37 +2,38 @@ package lc;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class ex119 {
 	public static void main(String[] args) {
-//		Scanner sc=new Scanner(System.in);
-//		int T=sc.nextInt();
-//		int[] nums=new int[T];
-//		int[] result=new int[2];
-//		int target=0;
-//		for(int i=0;i<T;i++) {
-//			nums[i]=sc.nextInt();
-//		}
-//		target=sc.nextInt();
-//		result=twoSum(nums,target);
-//		for(int i=0;i<T;i++) {
-//			System.out.println(result[i]);
-//		}
+		System.out.println(getRow(3));
 			
 	}
-	public List<List<Integer>> generate(int numRows)
+	
+    public static List<Integer> getRow(int rowIndex)
 	{
-		List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+		List<List<Integer>> allRows = new ArrayList<List<Integer>>();
 		ArrayList<Integer> row = new ArrayList<Integer>();
-		for(int i=0;i<numRows;i++)
+		for(int i=0;i<=rowIndex;i++)
 		{
-			row.add(0, 1);
-			for(int j=1;j<row.size()-1;j++)
+			row.add(0,1);
+			for(int j=1;j<row.size()-1;j++) {
 				row.set(j, row.get(j)+row.get(j+1));
-			allrows.add(new ArrayList<Integer>(row));
+			}
+			allRows.add(new ArrayList<Integer>(row));
 		}
-		return allrows;
-		
+		return allRows.get(rowIndex);
 	}
+//	public static List<List<Integer>> generate(int numRows)
+//	{
+//		List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+//		ArrayList<Integer> row = new ArrayList<Integer>();
+//		for(int i=0;i<numRows;i++)
+//		{
+//			row.add(0, 1);
+//			for(int j=1;j<row.size()-1;j++)
+//				row.set(j, row.get(j)+row.get(j+1));
+//			allrows.add(new ArrayList<Integer>(row));
+//		}
+//		return allrows;
+//	}
 }
