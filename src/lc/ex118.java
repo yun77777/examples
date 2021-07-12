c452c70b8fd18e60ca6241fceec5b62033c98153
yun77777 <victoriousyk@naver.com>
2021-07-12 19:52:40 +0900

    “m”

--- src/lc/ex118.java
+++ src/lc/ex118.java
@@ -1,34 +1,35 @@
 package lc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ex118 {
 	public static void main(String[] args) {
-//		Scanner sc=new Scanner(System.in);
-//		int T=sc.nextInt();
-//		int[] nums=new int[T];
-//		int[] result=new int[2];
-//		int target=0;
-//		for(int i=0;i<T;i++) {
-//			nums[i]=sc.nextInt();
-//		}
-//		target=sc.nextInt();
-//		result=twoSum(nums,target);
-//		for(int i=0;i<T;i++) {
-//			System.out.println(result[i]);
-//		}            
+		System.out.println(generate(2));
 			
 	}
-	public List<Integer> getRow(int k) {
-        Integer[] arr = new Integer[k + 1];
-        Arrays.fill(arr, 0);
-        arr[0] = 1;
-        
-        for (int i = 1; i <= k; i++) 
-            for (int j = i; j > 0; j--) 
-                arr[j] = arr[j] + arr[j - 1];
-        
-        return Arrays.asList(arr);
+    public static List<List<Integer>> generate(int numRows) {
+    	List<List<Integer>> allRows=new ArrayList<List<Integer>>();
+    	List<Integer> row=new ArrayList<Integer>();
+    	for (int i = 0; i < numRows; i++) {
+			row.add(0,1);
+			for (int j = 1; j < row.size()-1; j++) {
+				row.set(j, row.get(j)+row.get(j+1));
+			}
+			allRows.add(new ArrayList<Integer>(row));
+		}
+    	
+    	return allRows;
+    	
+//        Integer[] arr = new Integer[k + 1];
+//        Arrays.fill(arr, 0);
+//        arr[0] = 1;
+//        
+//        for (int i = 1; i <= k; i++) 
+//            for (int j = i; j > 0; j--) 
+//                arr[j] = arr[j] + arr[j - 1];
+//        
+//        return Arrays.asList(arr);
     }
 }
