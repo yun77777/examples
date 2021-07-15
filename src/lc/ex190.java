a60ba2dd6a2877e15d91da738ab830c2d3ecf624
yun77777 <victoriousyk@naver.com>
2021-07-15 20:44:03 +0900

    “m”

--- src/lc/ex190.java
+++ src/lc/ex190.java
@@ -4,22 +4,28 @@ import java.io.IOException;
 
 public class ex190 {
 	public static void main(String[] args) throws IOException {
-		System.out.println(reverseBits(5));
+		System.out.println(hammingWeight(5));
 	}
 	//00...0101
 	//1010...00
     // you need treat n as an unsigned value
-    public static int reverseBits(int n) {
+    public static int hammingWeight(int n) {
     	if(n==0) return 0;
-    	
-    	int result=0;
+    	int cnt=0;
     	for(int i=0;i<32;i++) {
-    		result<<=1;
-    		if((n&1)==1) result++;
+    		System.err.println("n:"+n+" n&1:"+(n&1));
+    		if((n&1)==1) cnt++;
     		n>>=1;
     	}
-    	
-    	return result;
+    	return cnt;
+//    	int result=0;
+//    	for(int i=0;i<32;i++) {
+//    		result<<=1;
+//    		if((n&1)==1) result++;
+//    		n>>=1;
+//    	}
+//    	
+//    	return result;
         
     }
 }
---
+++ src/lc/ex191.java
@@ -0,0 +1,25 @@
+package lc;
+
+import java.io.IOException;
+
+public class ex191 {
+	public static void main(String[] args) throws IOException {
+		System.out.println(reverseBits(5));
+	}
+	//00...0101
+	//1010...00
+    // you need treat n as an unsigned value
+    public static int reverseBits(int n) {
+    	if(n==0) return 0;
+    	
+    	int result=0;
+    	for(int i=0;i<32;i++) {
+    		result<<=1;
+    		if((n&1)==1) result++;
+    		n>>=1;
+    	}
+    	
+    	return result;
+        
+    }
+}
