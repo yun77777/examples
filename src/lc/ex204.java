25714144f026eaf65b8e8a520c8934197cdc29b8
yun77777 <victoriousyk@naver.com>
2021-07-16 22:29:45 +0900

    “m”

--- src/lc/ex204.java
+++ src/lc/ex204.java
@@ -1,44 +1,33 @@
 package lc;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ex204 {
 	public static void main(String[] args) throws IOException {
-		System.err.println(countPrimes(10));
+		System.err.println(isIsomorphic("egg","add"));
 	}
 
-	//에라토스테네스의 체 이용해서 소수 찾기
-    public static int countPrimes(int n) {
-    	boolean[] notPrime=new boolean[n];
-    	int cnt=0;
-    	for(int i=2;i<n;i++) {
-    		if(!notPrime[i]) {
-    			cnt++;
-    			for(int j=2;i*j<n;j++)
-    				notPrime[i*j]=true;
-    		}
-    	}
-		return cnt;
-	}
-//    public static int countPrimes(int n) {
-//    	if(n<2) return 0;
-//    	int cnt=0,i=2;
-//    	while(i!=n) {
-//    		if(isPrime(i))
-//    			cnt++;
-//    		i++;
-//    	}
-//    	System.err.println("cnt:"+cnt);
-//    	return cnt;
-//    }
-//    public static boolean isPrime(int n) {
-//    	int i=2;
-//    	while(i!=n) {
-//    		if(n%i==0) {
-//    			return false;
-//    		}
-//    		i++;
-//    	}
-//    	return true;
-//    }
+    public static boolean isIsomorphic(String s, String t) {
+    	HashMap<Character, Character> hm = new HashMap<Character, Character>();
+		
+ 	   /*looping through strig once and comparing 
+ 	   the characters if hashed and if not then mapping it.*/
+ 	   
+        for(int i=0; i<s.length(); i++){ 
+         
+             if(hm.containsKey(s.charAt(i))){
+                 if(hm.get(s.charAt(i))!=t.charAt(i))
+                     return false;}
+            
+             else if(hm.containsValue(t.charAt(i)))
+                     return false;
+            
+             else
+                hm.put(s.charAt(i), t.charAt(i));
+            
+           }
+            
+            return true;
+   }
 }
