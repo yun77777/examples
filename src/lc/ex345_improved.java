06d4ab64c8b7b076c17d288f86c872c5eb1a0328
yun77777 <victoriousyk@naver.com>
2021-07-22 01:12:21 +0900

    “m”

---
+++ src/lc/ex345_improved.java
@@ -0,0 +1,41 @@
+package lc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ex345_improved {
+	static int[] nums;
+
+	public static void main(String[] args) {
+		System.err.println(reverseVowels("Aa"));
+//		System.err.println(reverseVowels("hello"));
+	}
+	
+    public static String reverseVowels(String s) {
+    	if(s==null||s.length()==0) return s;
+//    	char[] vowels= {'a', 'e', 'i', 'o', 'u','A','E','I','O','U'};
+    	String vowels="aeiouAEIOU";
+    	char[] chars=s.toCharArray();
+    	int start=0;
+    	int end=s.length()-1;
+    	char temp=' ';
+    	
+    	while(start<end) {
+    		while(start<end && !vowels.contains(chars[start]+"")) {
+    			start++;
+    		}
+    		while(start<end && !vowels.contains(chars[end]+"")) {
+    			end--;
+    		}
+    		temp=chars[start];
+    		chars[start]=chars[end];
+    		chars[end]=temp;
+    		
+    		start++;
+    		end--;
+    	}
+    	return new String(chars);
+//    	return chars.toString();
+    	
+	}
+}
