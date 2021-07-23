c5ee504e759981c77a16f0d7b69f92e7460279ea
yun77777 <victoriousyk@naver.com>
2021-07-23 20:40:43 +0900

    “m”

---
+++ src/lc/ex387.java
@@ -0,0 +1,28 @@
+package lc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ex387 {
+	static int[] nums;
+
+	public static void main(String[] args) {
+		System.err.println(firstUniqChar("loveleetcode"));
+	}
+    public static int firstUniqChar(String s) {
+    	if(s==null) return -1;
+    	int[] arr=new int[26];
+    	List<Integer> list=new ArrayList<Integer>();
+    	for (int i = 0; i < s.length(); i++) {
+    		arr[s.charAt(i)-'a']++;
+//    		System.err.println("arr:"+arr[s.charAt(i)-'a']);
+		}
+    	for (int i = 0; i < s.length(); i++) {
+    		System.err.println(s.charAt(i)+":"+arr[s.charAt(i)-'a']);
+    		if(arr[s.charAt(i)-'a']==1)
+    			return i;
+    	}
+    	
+    	return -1;
+    }
+}
