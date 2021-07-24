602cf0e5ea367d460ce77d69ef8c6e0f15ec6cde
yun77777 <victoriousyk@naver.com>
2021-07-24 21:44:33 +0900

    “m”

--- src/lc/ex401.java
+++ src/lc/ex401.java
@@ -12,7 +12,7 @@ public class ex401 {
     	List<String> times = new ArrayList<>();
         for (int h=0; h<12; h++)
             for (int m=0; m<60; m++)
-                if (Integer.bitCount(h * 64 + m) == turnedOn)
+                if (Integer.bitCount(h * 64 + m) == turnedOn)//시간을 분 단위로 환
                     times.add(String.format("%d:%02d", h, m));
         return times; 
     }
