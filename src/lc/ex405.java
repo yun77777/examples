dfaf7cd1f61a5c389ea4e08fac3208e0437444d8
yun77777 <victoriousyk@naver.com>
2021-07-26 11:13:30 +0900

    “m”

---
+++ src/lc/ex405.java
@@ -0,0 +1,33 @@
+package lc;
+
+public class ex405 {
+	static int[] nums;
+
+	public static void main(String[] args) {
+		System.err.println(toHex(26));
+	}
+	public static String toHex(int num) {
+//		hexadecimal representation
+		String hex="0123456789abcdef";
+		String res="";
+		char[] hexArr=hex.toCharArray();
+		while(num!=0) {
+			res+=hexArr[(num&15)];
+			num>>>=4;
+		}
+		return res;
+    }
+	
+	
+//	public static String toHex(int num) {
+//		if(num==0) return "0";
+//		StringBuilder res=new StringBuilder();
+//		int digit=0;
+//		while(num!=0) {
+//			digit=num&0xf;
+//			res.append(digit<10 ? (char)(digit+'0') : (char)(digit-10+'a'));
+//			num>>>=4;
+//		}
+//		return res.reverse().toString();
+//	}
+}
