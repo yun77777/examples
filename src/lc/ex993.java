ea9e5153678288cbecaa62fc438a55efb5164c2b
yun77777 <victoriousyk@naver.com>
2021-07-31 16:15:40 +0900

    “m”

---
+++ src/lc/ex993.java
@@ -0,0 +1,30 @@
+package lc;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ex993 {
+    public boolean isCousins(TreeNode root, int x, int y) {
+    	//BFS
+    	if(root==null)return false;
+    	Queue<TreeNode> queue=new LinkedList<TreeNode>();
+    	queue.offer(root);
+    	while(!queue.isEmpty()) {
+    		int size=queue.size();
+    		boolean isAexist=false;
+    		boolean isBexist=false;
+    		for(int i=0;i<size;i++) {
+    			TreeNode cur=queue.poll();
+    			if(cur.val==x)isAexist=true;
+    			if(cur.val==y)isBexist=true;
+    			if(cur.left!=null&cur.right!=null) {
+    				if((cur.left.val==x&&cur.right.val==y)||(cur.left.val==y&&cur.right.val==x)) return false;
+    			}
+    			if(cur.left!=null) queue.offer(cur.left);
+    			if(cur.right!=null) queue.offer(cur.right);
+    		}
+    		if(isAexist&&isBexist) return true;
+    	}
+    	return false;
+	}
+}
