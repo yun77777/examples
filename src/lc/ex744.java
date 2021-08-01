aefe1adbcde92b6ac6b170c42fe9edfc9dff90b8
yun77777 <victoriousyk@naver.com>
2021-08-01 11:47:50 +0900

    “m”

---
+++ src/lc/ex744.java
@@ -0,0 +1,11 @@
+package lc;
+
+public class ex744 {
+	public char nextGreatestLetter(char[] letters, char target) {
+		for(char c: letters) {
+			if(c>target)
+				return c;
+		}
+		return letters[0];
+	}
+}
