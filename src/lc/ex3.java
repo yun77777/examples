fc31044937922552873fd898b6e763a1c062f4a0
yun77777 <victoriousyk@naver.com>
2021-08-01 18:21:33 +0900

    “m”

--- src/lc/ex3.java
+++ src/lc/ex3.java
@@ -20,9 +20,22 @@ public class ex3 {
     		if(!set.contains(s.charAt(j))) {
     			set.add(s.charAt(j++));
     			max=Math.max(max, set.size());
-    		} else
+    		} else {
     			set.remove(s.charAt(i++));
+    		}
     	}
     	return max;
     }
+//    public static int lengthOfLongestSubstring(String s) {
+//    	int i=0,j=0,max=0;
+//    	Set<Character> set=new HashSet<>();
+//    	while(j<s.length()) {
+//    		if(!set.contains(s.charAt(j))) {
+//    			set.add(s.charAt(j++));
+//    			max=Math.max(max, set.size());
+//    		} else
+//    			set.remove(s.charAt(i++));
+//    	}
+//    	return max;
+//    }
 }
