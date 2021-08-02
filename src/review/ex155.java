1f465c104c69754d5b3069204639f535e2760257
yun77777 <victoriousyk@naver.com>
2021-08-02 19:55:58 +0900

    “m”

---
+++ src/review/ex155.java
@@ -0,0 +1,24 @@
+//package review;
+//
+//
+//public class ex155 {
+//	private Node node;
+//    public ex155() {
+//    }
+//    
+//    public void push(int val) {
+//        
+//    }
+//    
+//    public void pop() {
+//        
+//    }
+//    
+//    public int top() {
+//        
+//    }
+//    
+//    public int getMin() {
+//        
+//    }
+//}
---
+++ src/review/ex20.java
@@ -0,0 +1,21 @@
+package review;
+
+import java.util.Stack;
+
+public class ex20 {
+	public static void main(String[] args) {
+		System.err.println(isValid("(([))"));
+	}
+    public static boolean isValid(String s) {
+    	if(s.length()==0) return false;
+    	Stack<Character> stack=new Stack<Character>();
+    	for(char c:s.toCharArray()) {
+    		if(c=='(') stack.push(')');
+    		else if(c=='{') stack.push('}');
+    		else if(c=='[') stack.push(']');
+    		else if(stack.isEmpty()||stack.pop()!=c)
+    			return false;
+    	}
+    	return stack.isEmpty();
+    }
+}
