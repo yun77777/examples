c5148e6265c659c66e60434409ba1e84ad74ee3c
yun77777 <victoriousyk@naver.com>
2021-08-05 13:17:52 +0900

    “m”

--- src/prgrmrs/binary_search_ex1.java
+++ src/prgrmrs/binary_search_ex1.java
@@ -1,6 +1,6 @@
 package prgrmrs;
 
-import java.util.Stack;
+import java.util.Arrays;
 
 public class binary_search_ex1 {
 
@@ -10,23 +10,41 @@ public class binary_search_ex1 {
 	}
 	
 	public static long solution(int n, int[] times) {
-        long answer = 0;
-        Stack<Integer> stack=new Stack<>();
-        for(int j=0;j<n;j++) {
-	        for (int i = times.length-1; i >= 0; i--) {
-				stack.push(times[i]);
-			}
-        }
-        while(!stack.isEmpty()) {
-        	//7 10
-        	//7(1) 14 21
-        	//10 20
-        	
-        	int num=stack.pop();
-        	System.err.println("NUM:"+num);
-        	answer+=num;
-        }
-        
-        return answer;
+       long answer = 0;
+       Arrays.sort(times);
+       long left=1;
+       long right=(long)n*times[times.length-1];
+       while(left<=right) {
+    	   long mid=(left+right)/2;
+    	   long sum=0;
+    	   for(int i=0;i<times.length;i++) sum+=mid/times[i];
+    	   if(sum<n) left=mid+1;
+    	   else {
+    		   right=mid-1;
+    		   answer=mid;
+    	   }
+       }
+       return answer;
     }
+	
+//	public static long solution(int n, int[] times) {
+//		long answer = 0;
+//		
+//		Arrays.sort(times);
+//		long left=0;//1분 이상 1,000,000,000분 이하 시간 범위
+//		long right=(long)n*times[times.length-1];//최악의 경우 최대 소요 시간
+//		while(left<=right) {//이분 탐색
+//			long mid=(left+right)/2;
+//			long sum=0;//심사관 수
+//			for(int i=0;i<times.length;i++) sum+=mid/times[i];//심사관 수=mid/소요시간
+//			if(sum<n) left=mid+1;
+//			else {
+//				right=mid-1;
+//				answer=mid;
+//			}
+//		}
+//		
+//		return answer;
+//	}
+	
 }
