c676497e8c27a7a9dbcf3e4e20d4c79076604424
yun77777 <victoriousyk@naver.com>
2021-08-05 13:46:24 +0900

    “m”

--- src/prgrmrs/binary_search_ex1.java
+++ src/prgrmrs/binary_search_ex1.java
@@ -10,22 +10,40 @@ public class binary_search_ex1 {
 	}
 	
 	public static long solution(int n, int[] times) {
-       long answer = 0;
-       Arrays.sort(times);
-       long left=1;
-       long right=(long)n*times[times.length-1];
-       while(left<=right) {
-    	   long mid=(left+right)/2;
-    	   long sum=0;
-    	   for(int i=0;i<times.length;i++) sum+=mid/times[i];
-    	   if(sum<n) left=mid+1;
-    	   else {
-    		   right=mid-1;
-    		   answer=mid;
-    	   }
-       }
-       return answer;
+        long answer = 0;
+        Arrays.sort(times);
+        long left=1;
+        long right=(long)n*times[times.length-1];
+        while(left<=right){
+            long mid=(left+right)/2;
+            long sum=0;
+            for(int i=0;i<times.length;i++) sum+=mid/times[i];
+            if(sum<n) left=mid+1;
+            else{
+                right=mid-1;
+                answer=mid;
+            }
+        }
+        return answer;
     }
+	
+//	public static long solution(int n, int[] times) {
+//		long answer = 0;
+//		Arrays.sort(times);
+//		long left=1;
+//		long right=(long)n*times[times.length-1];
+//		while(left<=right) {
+//			long mid=(left+right)/2;
+//			long sum=0;
+//			for(int i=0;i<times.length;i++) sum+=mid/times[i];
+//			if(sum<n) left=mid+1;
+//			else {
+//				right=mid-1;
+//				answer=mid;
+//			}
+//		}
+//		return answer;
+//	}
 	
 //	public static long solution(int n, int[] times) {
 //		long answer = 0;
