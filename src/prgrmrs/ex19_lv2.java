f5ad18847ad6ecbded0ba7510f050a9e71897955
yun77777 <victoriousyk@naver.com>
2021-08-10 14:59:11 +0900

    “m”

--- src/prgrmrs/ex19_lv2.java
+++ src/prgrmrs/ex19_lv2.java
@@ -10,38 +10,37 @@ public class ex19_lv2 {
 		String numbers="17";
 		System.err.println(solution(numbers));
 	}
-	
-	public static Set<Integer> set;
+	static Set<Integer> set;
 	public static int solution(String numbers) {
-		set=new HashSet<Integer>();
+		set=new HashSet<>();
 		for(int i=0; i<numbers.length(); i++) {
 			permutation(numbers.split(""),0,numbers.length(),i+1);
 		}
-		System.err.println(set);
 		return isPrime();
 	}
-	public static void permutation(String[] arr, int depth, int n, int r) {
-		if(depth==r) {//문자열 이어 붙임
+	
+	public static void permutation(String[] str, int depth, int n, int r) {
+		if(depth==r) {
 			StringBuilder sb=new StringBuilder();
-			for(int i=0; i<r; i++) sb.append(arr[i]);
+			for(int i=0; i<r; i++) sb.append(str[i]);
 			set.add(Integer.parseInt(sb.toString()));
 			System.err.println(set);
 			return;
 		}
 		for(int i=depth; i<n; i++) {
-			swap(arr,i,depth);//문자열 뒤집음
-			permutation(arr,depth+1,n,r);
-			swap(arr,i,depth);
+			swap(str,i,depth);
+			permutation(str,depth+1,n,r);
+			swap(str,i,depth);
 		}
 	}
 	
-	public static void swap(String[] arr, int i, int j) {
-		String temp=arr[i];
-		arr[i]=arr[j];
-		arr[j]=temp;
+	public static void swap(String[] str, int i, int j) {
+		String temp=str[i];
+		str[i]=str[j];
+		str[j]=temp;
 	}
 	
-	public static int isPrime() {//소수 개수 계산
+	public static int isPrime() {
 		int count=0;
 		Iterator<Integer> iter=set.iterator();
 		while(iter.hasNext()) {
@@ -49,14 +48,87 @@ public class ex19_lv2 {
 			int num=iter.next();
 			iter.remove();
 			if(num==0 || num==1) continue;
-			for(int i=num; i<=Math.sqrt(num); i++) {
-				if(num%i==0) {
+			for(int i=2; i<=Math.sqrt(num); i++) {
+				if(i%num==0) {
 					flag=false;
 					break;
 				}
 			}
 			if(flag) count++;
 		}
-		return count;
+		return  count;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	public static Set<Integer> set;
+//	public static int solution(String numbers) {
+//		set=new HashSet<Integer>();
+//		for(int i=0; i<numbers.length(); i++) {
+//			permutation(numbers.split(""),0,numbers.length(),i+1);
+//		}
+//		System.err.println(set);
+//		return isPrime();
+//	}
+//	public static void permutation(String[] arr, int depth, int n, int r) {
+//		if(depth==r) {//문자열 이어 붙임
+//			StringBuilder sb=new StringBuilder();
+//			for(int i=0; i<r; i++) sb.append(arr[i]);
+//			set.add(Integer.parseInt(sb.toString()));
+//			System.err.println(set);
+//			return;
+//		}
+//		for(int i=depth; i<n; i++) {
+//			swap(arr,i,depth);//문자열 뒤집음
+//			permutation(arr,depth+1,n,r);
+//			swap(arr,i,depth);
+//		}
+//	}
+//	
+//	public static void swap(String[] arr, int i, int j) {
+//		String temp=arr[i];
+//		arr[i]=arr[j];
+//		arr[j]=temp;
+//	}
+//	
+//	public static int isPrime() {//소수 개수 계산
+//		int count=0;
+//		Iterator<Integer> iter=set.iterator();
+//		while(iter.hasNext()) {
+//			boolean flag=true;
+//			int num=iter.next();
+//			iter.remove();
+//			if(num==0 || num==1) continue;
+//			for(int i=num; i<=Math.sqrt(num); i++) {
+//				if(num%i==0) {
+//					flag=false;
+//					break;
+//				}
+//			}
+//			if(flag) count++;
+//		}
+//		return count;
+//	}
 }
---
+++ src/prgrmrs/ex20_lv2.java
@@ -0,0 +1,34 @@
+package prgrmrs;
+
+import java.util.Arrays;
+
+public class ex20_lv2 {
+
+	public static void main(String[] args) {
+		System.out.println(solution("4177252841", 4));
+	}
+
+	public static String solution(String number, int k) {
+		StringBuilder sb=new StringBuilder();
+		int max=-1;
+		int left=0;
+		int cnt=number.length()-k;
+		int right=number.length()-cnt;
+		int idx=0;
+		while(cnt>0) {
+			max=-1;
+			for(int i=left; i<=right; i++) {
+				int num=number.charAt(i)-'0';
+				if(num>max) {
+					max=num;
+					idx=i;
+				}
+			}
+			sb.append(max);
+			left=idx+1;
+			right=number.length()- --cnt;
+		}
+		
+		return sb.toString();
+	}
+}
