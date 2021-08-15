2a3c43d8edcac9a95efab8a1e6bff823bdda1008
yun77777 <victoriousyk@naver.com>
2021-08-15 10:13:32 +0900

    “m”

---
+++ src/prgrmrs/multiplyMatrix_lv2.java
@@ -0,0 +1,27 @@
+package prgrmrs;
+
+public class multiplyMatrix_lv2 {
+
+	public static void main(String[] args) {	
+		int[][] arr1= {{1,4},{3,2},{4,1}};
+		int[][] arr2= {{3,3},{3,3}};
+		int[][] res=solution(arr1, arr2);
+		for (int i = 0; i < res.length; i++) {
+			for (int j = 0; j < res[0].length; j++) {
+				System.err.println(res[i][j]);
+			}
+		}
+	}
+
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr2[0].length; j++) {
+				for (int k = 0; k < arr2.length; k++) {
+					answer[i][j]+=arr1[i][k]*arr2[k][j];
+				}
+			}
+		}
+		return answer;
+	}
+}
