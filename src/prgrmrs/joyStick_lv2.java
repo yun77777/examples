dafa3724af8482780bc42862bc5521dd9de4a146
yun77777 <victoriousyk@naver.com>
2021-08-23 20:42:06 +0900

    “m”

---
+++ src/prgrmrs/joyStick_lv2.java
@@ -0,0 +1,13 @@
+package prgrmrs;
+
+public class joyStick_lv2 {
+
+	public static void main(String[] args) {
+		System.err.println(solution("JEROEN"));
+	}
+
+	public static int solution(String name) {
+		int answer = 0;
+		return answer;
+	}
+}
